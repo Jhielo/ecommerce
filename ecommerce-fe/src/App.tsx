ef70197e0881b69117d7c04afc88d3004497371d
Lois Gonzales <loisjersong@gmail.com>
2025-07-15 12:59:09 +0800

    chore: setup shadcn ui

--- ecommerce-fe/src/App.tsx
+++ ecommerce-fe/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen min-w-screen bg-gray-100">
-        <span className="text-lg font-medium">Hello World</span>
+        <Button variant="destructive"> Hello World </Button>
       </div>
     </>
   )
